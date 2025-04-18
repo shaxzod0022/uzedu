f67434094a22a87797395f25f6c3ccc1f92dfcb1
shaxzod0022 <ilgeldiyevshaxzod@gmail.com>
2025-04-18 14:47:13 +0500

    content

--- src/util/styles.js
+++ src/util/styles.js
@@ -1,11 +1,6 @@
 export const styles = {
-  container: "max-w-[1700px] w-full px-[20px] md:px-[60px] lg:px-[100px]",
   paragraph: "lg:text-[18px] text-[15px] font-normal font-figtree",
-  paddingY: "md:py-5 py-2",
-  paddingX: "md:px-20 px-10",
-  input: "w-full outline-none border-2 p-2 rounded-md border-blue-500/40",
-  heading1:
-    "font-bold md:text-3xl text-[26px]",
+  heading1: "font-bold md:text-3xl text-[26px]",
   heading2:
     "font-bold md:text-[26px] text-[20px] md:leading-[36px] leading-[30px]",
   heading3:
@@ -18,6 +13,6 @@ export const styles = {
   fWrap: "flex flex-wrap items-center",
   fArround: "flex justify-around items-center flex-wrap",
   button:
-    "bg-blue-500 py-2 px-6 text-white rounded-sm font-semibold font-figtree tracking-wide hover:bg-blue-600 active:bg-blue-400 sm:text-[16px] text-[14px]",
+    "bg-myGreen py-2 px-6 text-white rounded-md font-semibold font-figtree tracking-wide hover:bg-yellow-300 sm:text-[16px] text-[14px]",
   link: "text-white text-[18px] hover:text-slate-400 cursor-pointer",
 };
