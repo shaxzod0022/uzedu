f09dd4c7a7158ecb77e299fee88a2a8e2973133f
nurmuhammadxon <muhiddinovnurmuhammadxon1@gmail.com>
2025-04-18 23:01:37 +0500

    qayta push

--- src/components/EducationSuggestions.jsx
+++ src/components/EducationSuggestions.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react'
-import { regions } from '../util/constants'
+import React, { useState } from 'react';
+import { regions } from '../util/constants';
 
 function EducationSuggestions() {
-    const [selectedRegion, setSelectedRegion] = useState('');
-    const [selectedDistrict, setSelectedDistrict] = useState('');
     const [value, setValue] = useState({
         name: '',
         region: '',
@@ -11,68 +9,79 @@ function EducationSuggestions() {
         phoneNumber: '',
         direction: '',
         suggestions: ''
+    });
+
+    const [errorValue, setErrorValue] = useState({
+        name: false,
+        region: false,
+        district: false,
+        phoneNumber: false,
+        direction: false,
+        suggestions: false
     })
-    const handleRegionChange = (e) => {
-        setSelectedRegion(e.target.value);
-        setSelectedDistrict('');
-    };
 
-    const handleDistrictChange = (e) => {
-        setSelectedDistrict(e.target.value);
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setValue((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
     };
-
-    const region = regions.find((r) => r.value === selectedRegion);
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const form = e.target;
+        console.log('Form Data:', value);
 
-        if (!form.checkValidity()) {
-            form.reportValidity();
-            return;
-        }
-
+        setValue({
+            name: '',
+            region: '',
+            district: '',
+            phoneNumber: '',
+            direction: '',
+            suggestions: ''
+        });
     };
+
+    const region = regions.find((r) => r.value == value.region);
+
     return (
         <div className='pt-7 max-w-[700px] w-full mx-auto bg-white'>
             <h1 className='text-3xl my-2.5 font-bold text-myGreen text-center relative z-10 educationTitle__aft'>
                 Ta'lim sifatini oshirishga qaratilgan murojaat va takliflarni yuboring
             </h1>
             <div className='w-full'>
-                <form
-                    className='flex flex-wrap w-full pt-7'
-                    onSubmit={handleSubmit}
-                >
+                <form onSubmit={handleSubmit} className='flex flex-wrap w-full pt-7'>
                     <div className='w-1/2 pr-2'>
                         <div className='mb-7'>
                             <input
                                 type="text"
+                                name="name"
+                                value={value.name}
+                                onChange={handleInputChange}
                                 className='px-3 py-4 text-base font-normal border border-[#151d41] text-[#495057] bg-white rounded outline-none w-full h-[calc(1.5em + 0.75rem + 2px)]'
                                 placeholder='F.I.SH'
                                 required
-                                value={value}
-                                onChange={(e) => setValue(e.target.value)}
                             />
-
                         </div>
                         <div className='mb-7'>
                             <input
                                 type="text"
+                                name="phoneNumber"
+                                value={value.phoneNumber}
+                                onChange={handleInputChange}
                                 className='px-3 py-4 text-base font-normal border border-[#151d41] text-[#495057] bg-white rounded outline-none w-full h-[calc(1.5em + 0.75rem + 2px)]'
                                 placeholder='Telefon'
-                                value={value}
-                                onChange={(e) => setValue(e.target.value)}
                             />
                         </div>
                     </div>
                     <div className='w-1/2 pl-2'>
                         <div className='flex space-x-4 mb-7'>
-                            {/* hudular */}
                             <div className='w-1/2'>
                                 <select
-                                    onChange={handleRegionChange}
-                                    value={selectedRegion}
+                                    name="region"
+                                    value={value.region}
+                                    onChange={handleInputChange}
                                     className='px-3 py-4 text-base font-normal border border-[#151d41] text-[#495057] bg-white rounded outline-none w-full'
                                     required
                                 >
@@ -84,16 +93,16 @@ function EducationSuggestions() {
                                     ))}
                                 </select>
                             </div>
-                            {/* tumanlar */}
                             <div className='w-1/2'>
                                 <select
-                                    onChange={handleDistrictChange}
-                                    value={selectedDistrict}
-                                    required
+                                    name="district"
+                                    value={value.district}
+                                    onChange={handleInputChange}
                                     className='px-3 py-4 text-base font-normal border border-[#151d41] text-[#495057] bg-white rounded outline-none w-full'
+                                    required
                                 >
                                     <option value="">Tumanlar</option>
-                                    {selectedRegion && region &&
+                                    {value.region && region &&
                                         region.tumanlar.map((district) => (
                                             <option key={district.id} value={district.value}>
                                                 {district.sub__title}
@@ -105,12 +114,13 @@ function EducationSuggestions() {
                         </div>
                         <div className='mb-7'>
                             <select
-                                onChange={handleDistrictChange}
-                                value={selectedDistrict}
-                                required
+                                name="direction"
+                                value={value.direction}
+                                onChange={handleInputChange}
                                 className='px-3 py-4 text-base font-normal border border-[#151d41] text-[#495057] bg-white rounded outline-none w-full'
+                                required
                             >
-                                <option>Yo'nalishni tanlang</option>
+                                <option value="">Yo'nalishni tanlang</option>
                                 <option value="1">Maktabgacha ta'lim</option>
                                 <option value="2">Maktab ta'lim</option>
                             </select>
@@ -118,24 +128,22 @@ function EducationSuggestions() {
                     </div>
                     <div className='w-full mb-7'>
                         <textarea
+                            name="suggestions"
                             rows={5}
+                            value={value.suggestions}
+                            onChange={handleInputChange}
                             placeholder="Murojaat va takliflar"
                             className='p-4 m-0 text-base font-normal border border-[#151d41] text-[#495057] bg-white rounded outline-none w-full'
-                            value={value}
-                            onChange={(e) => setValue(e.target.value)}
                         ></textarea>
                     </div>
                     <button
                         type='submit'
                         className='text-lg text-white bg-myGreen py-2.5 px-10 rounded-md mx-auto hover:bg-primary hover:text-black transition-all ease-linear duration-200'
-                        onClick={(e) => handleSubmit(e)}
-                    >
-                        Yuborish
-                    </button>
+                    >Yuborish</button>
                 </form>
             </div>
         </div>
-    )
+    );
 }
 
-export default EducationSuggestions
+export default EducationSuggestions;
