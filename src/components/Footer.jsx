cd36025b56c8fe42b49f5be3455fe844b3ad7b33
nurmuhammadxon <muhiddinovnurmuhammadxon1@gmail.com>
2025-04-18 14:13:42 +0500

    footer page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,69 +1,93 @@
-import React from "react";
-import { logo } from "../assets";
-import { styles } from "../util/styles";
-import { useNavigate } from "react-router";
-import { footerLinks } from "../util/constants";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom';
 
-const Footer = () => {
-  const navigate = useNavigate();
+// logo 
+import logoSecondary from '../assets/logoSecondary.svg'
+import logoSecondaryDark from '../assets/logoSecondaryDark.svg'
+
+// icons import 
+import { Phone, Mail, Facebook, Send, Instagram, Youtube } from 'lucide-react';
+
+function Footer() {
+
+  // network list
+  const networkList = [
+    {
+      id: 1,
+      icon: <Phone />,
+      title: '(71) 202 09 09',
+      link: '#'
+    },
+    {
+      id: 2,
+      icon: <Mail />,
+      title: 'info@uzedu.uz',
+      link: '#'
+    },
+    {
+      id: 3,
+      icon: <Facebook />,
+      title: '@uzedu',
+      link: '#'
+    },
+    {
+      id: 4,
+      icon: <Send />,
+      title: '@uzedu',
+      link: '#'
+    },
+    {
+      id: 5,
+      icon: <Instagram />,
+      title: '/uzedu',
+      link: '#'
+    },
+    {
+      id: 6,
+      icon: <Youtube />,
+      title: '/uzedu',
+      link: '#'
+    },
+
+  ]
+
   return (
-    <div className={`bg-blue-500 ${styles.container} pt-8 pb-3`}>
-      <div
-        className={`${styles.fArround} jus !items-start flex-wrap gap-10 mb-6`}
-      >
-        <div
-          className={`${styles.fStart} !justify-center cursor-pointer flex-wrap gap-4`}
-          onClick={() => navigate("/home_page")}
-        >
-          <div className={`w-[150px] flex sm:mb-0 justify-center`}>
-            <img
-              className="rounded-full sm:w-[70px] w-[50px]"
-              src={logo}
-              alt="maktap platformasi logotipi"
-            />
+    <footer
+      className='min-h-[600px] h-full relative overflow-hidden pb-[30px] text-white'
+    >
+      <div className='relative flex pt-[188px] bg-myPurple z-10'>
+        <div className='w-[418px] pr-[100px] ml-[125px] border-r border-r-white'>
+          <img
+            src={logoSecondary}
+            alt="logo"
+            className='w-full mb-7' />
+          <h1 className='text-xl font-bold mb-9'>
+            O‘zbekiston Respublikasi maktabgacha va maktab ta'limi vazirligi
+          </h1>
+          <h2 className='mb-12 text-xl font-thin'>
+            100011, O‘zbekiston, Toshkent, Shayxontohur tumani, Navoiy ko‘chasi, 2A-uy
+          </h2>
+          <div>
+            <ul>
+              {
+                networkList.map(item => (
+                  <li key={item.id}>
+                    <Link
+                      to={item.link}
+                      className='text-lg text-white'
+                    >
+                      {item.icon}
+                      <span>{item.title}</span>
+                    </Link>
+                  </li>
+                ))
+              }
+            </ul>
           </div>
         </div>
-        <div className={`sm:w-[50%] w-full !items-start flex-wrap gap-10`}>
-          {
-            <div
-              className={`${styles.fCol} sm:items-start items-center text-start sm:w-auto w-full`}
-            >
-              <h3 className={`${styles.heading3} text-white mb-3`}>
-                {footerLinks.title}
-              </h3>
-              <ul
-                className={`${styles.fBetween} gap-6 sm:items-start items-center`}
-              >
-                {footerLinks.links.map((link, linkidx) => {
-                  return (
-                    <li key={linkidx} className={`${styles.link}`}>
-                      <Link
-                        to={link.url}
-                        className={`${styles.fStart} gap-3 cursor-pointer sm:text-[16px] text-[14px] hover:text-slate-500`}
-                        target="blank"
-                      >
-                        <img
-                          className="w-[40px] h-[40px] hover:scale-110 rounded-full"
-                          src={link.linkTitle}
-                          alt=""
-                        />
-                        <span className="font-semibold">{link.title}</span>
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          }
-        </div>
       </div>
-      <div className="w-full bg-white h-0.5 mb-3" />
-      <div className="text-center mb-2">
-        <span className={`${styles.span} text-white`}>Copyright@ 2025.</span>
-      </div>
-    </div>
-  );
-};
+    </footer>
+  )
+}
 
-export default Footer;
+export default Footer
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Head } from "../components";
+import { Footer, Head } from "../components";
 
 const Home = () => {
   return (
     <div>
       <Head />
+      <Footer />
     </div>
   );
 };
