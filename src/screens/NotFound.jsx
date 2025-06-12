f7257b1778d70b8bcaeb666760714ab0dab7b6d1
shaxzod0022 <ilgeldiyevshaxzod@gmail.com>
2025-06-12 10:45:09 +0500

    not found

--- src/screens/NotFound.jsx
+++ src/screens/NotFound.jsx
@@ -8,7 +8,7 @@ const NotFound = () => {
       <Navbar newClass={"bg-myGreen w-full lg:!px-36 sm:w-full"} />
       <div className="mx-auto p-4 sm:px-6 lg:px-8 xxl:w-full sm:w-[80%] w-full">
         <div className="mb-5">
-          <h2 className={`${styles.heading2}`}>Yangiliklar</h2>
+          <h2 className={`${styles.heading2}`}></h2>
           <p className="bg-yellow-300 w-[80px] h-[5px]"></p>
         </div>
         <h2 className={`${styles.heading2} text-center py-10`}>Ma'lumotlar tayyorlanmoqda</h2>
